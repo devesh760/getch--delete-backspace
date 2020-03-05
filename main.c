761a80421292dd07dc9439c3f46547d9cff950a2
devesh760 <60389654+devesh760@users.noreply.github.com>
2020-03-05 17:57:01 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,29 @@
+#include<stdio.h> 
+int main() 
+{ 
+int i=0;
+char ch[100];
+printf("Enter your password:");
+for(;;) 
+{
+ch[i]=_getch();
+if(ch[i]=='\r')
+break;
+else if(ch[i]=='\b') 
+{ 
+if(i!=0) 
+{ 
+i=i-1;
+printf("\b \b");
+}
+} 
+else
+{ 
+printf("%c",ch[i]);
+i++;
+}
+} 
+ch[i]=NULL;
+printf("\n%s",ch);
+return 0;
+}
